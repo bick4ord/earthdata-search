bd024960285edb5cc8785900fefe463a617bad4a
Ryan Bickford <rbickford@element84.com>
2024-12-02 09:42:55 -0500

    add favicon to deploy and automate pulling static content from docker image

--- deploy-site.sh
+++ deploy-site.sh
@@ -2,9 +2,23 @@
 
 BUCKET=${1}
 
+rm -rf static/dist
+mkdir -p static/dist
+
+docker run -it \
+  --platform linux/amd64 \
+  --entrypoint '' \
+  -v $(pwd)/static:/static earthdata-search:deploy \
+  bash -exc "cp -r /build/static/dist/* /static/dist"
+
 function s3_copy {
+       if [ "${3}" == "" ]; then
+         dir=dist
+       else
+         dir=${3}
+       fi
        aws s3 cp \
-              ./website \
+              ./static/${dir}\
               s3://${BUCKET}/ \
               --exclude '*' \
               --include "*.${1}" \
@@ -19,3 +33,4 @@ s3_copy js "text/javascript; charset=utf-8"
 s3_copy png image/png
 s3_copy svg image/svg+xml
 s3_copy css "text/css; charset=utf-8"
+s3_copy ico image/x-icon src/public
